62c4975b73653f252c5cb98e0ede4dc5b13f9319
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-03-03 11:44:07 -0800

    Add category functionality
    Co-Authored-By: Chase Stokes <chasehstokes@gmail.com>
    Co-authored-by: Matt Eilar <meilar@gmail.com>

---
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -0,0 +1,77 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class CategoryTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Category.ClearAll();
+    }
+    [TestMethod]
+    public void CategoryConstructor_CreatesInstanceOfCategory_Category()
+    {
+      Category newCategory = new Category("test category");
+      Assert.AreEqual(typeof(Category), newCategory.GetType());
+    }
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+    string name = "Test Category";
+    Category newCategory = new Category(name);
+    string result = newCategory.Name;
+    Assert.AreEqual(name, result);
+    }
+    [TestMethod]
+    public void GetId_ReturnsCategoryId_int()
+    {
+      string name = "Test Category";
+      Category newCategory = new Category(name);
+      int result = newCategory.Id;
+      Assert.AreEqual(1, result);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsAllCategoryObjects_CategoryList()
+    {
+      string name01 = "Work";
+      string name02 = "School";
+      Category newCategory1 = new Category(name01);
+      Category newCategory2 = new Category(name02);
+      List<Category> newList = new List<Category> { newCategory1, newCategory2 };
+      List<Category> result = Category.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+    [TestMethod]
+    public void Find_ReturnsCorrectCategory_Category()
+    {
+      string name01 = "Work";
+      string name02 = "School";
+      Category newCategory1 = new Category(name01);
+      Category newCategory2 = new Category(name02);
+      Category result = Category.Find(2);
+      Assert.AreEqual(newCategory2, result);
+    }
+
+    [TestMethod]
+    public void AddItem_AssociatesItemWithCategory_ItemList()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      List<Item> newList = new List<Item> { newItem };
+      string name = "Work";
+      Category newCategory = new Category(name);
+      newCategory.AddItem(newItem);
+
+      //Act
+      List<Item> result = newCategory.Items;
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+  }
+}
