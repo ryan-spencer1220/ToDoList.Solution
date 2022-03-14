731e2e6fac6dd6fc22bb241ce8a8e1866928c13f
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-03-14 09:18:29 -0700

    Add funcitonality to allow many-to-many relationships between items and categories

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -59,18 +59,13 @@ namespace ToDoList.Tests
     [TestMethod]
     public void AddItem_AssociatesItemWithCategory_ItemList()
     {
-      //Arrange
       string description = "Walk the dog.";
       Item newItem = new Item(description);
       List<Item> newList = new List<Item> { newItem };
       string name = "Work";
       Category newCategory = new Category(name);
       newCategory.AddItem(newItem);
-
-      //Act
       List<Item> result = newCategory.Items;
-
-      //Assert
       CollectionAssert.AreEqual(newList, result);
     }
   }
--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,35 +1,77 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
+    private readonly ToDoListContext _db;
 
-    [HttpGet("/categories/{categoryId}/items/new")]
-    public ActionResult New(int categoryId)
+    public ItemsController(ToDoListContext db)
     {
-      Category category = Category.Find(categoryId);
-      return View(category);
+      _db = db;
     }
 
-    [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
+    public ActionResult Index()
     {
-      Item.ClearAll();
-      return View();
+    return View(_db.Items.ToList());
     }
 
-    [HttpGet("/categories/{categoryId}/items/{itemId}")]
-    public ActionResult Show(int categoryId, int itemId)
+    // public ActionResult Create()
+    // {
+    //   ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+    //   return View();
+    // }
+
+    // [HttpPost]
+    // public ActionResult Create(Item item)
+    // {
+    //   _db.Items.Add(item);
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
+
+    public ActionResult Details(int id)
     {
-      Item item = Item.Find(itemId);
-      Category category = Category.Find(categoryId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("item", item);
-      model.Add("category", category);
-      return View(model);
+        var thisItem = _db.Items
+            .Include(item => item.JoinEntities)
+            .ThenInclude(join => join.Category)
+            .FirstOrDefault(item => item.ItemId == id);
+        return View(thisItem);
     }
+
+    // public ActionResult Edit(int id)
+    // {
+    //   var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+    //   ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+    //   return View(thisItem);
+    // }
+
+    // [HttpPost]
+    // public ActionResult Edit(Item item)
+    // {
+    //   _db.Entry(item).State = EntityState.Modified;
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
+
+    // public ActionResult Delete(int id)
+    // {
+    //   var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+    //   return View(thisItem);
+    // }
+
+    // [HttpPost, ActionName("Delete")]
+    // public ActionResult DeleteConfirmed(int id)
+    // {
+    //   var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+    //   _db.Items.Remove(thisItem);
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
   }
 }
