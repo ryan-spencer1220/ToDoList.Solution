f0d8ed4cf780e4e845d21d8f78dd6fd2f8c04408
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-03-14 13:13:53 -0700

    Add completed and date options to item creation

--- ToDoList/Migrations/20220314184536_Initial.cs
+++ ToDoList/Migrations/20220314184536_Initial.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace ToDoList.Migrations
@@ -27,32 +28,61 @@ namespace ToDoList.Migrations
                     ItemId = table.Column<int>(type: "int", nullable: false)
                         .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
                     Description = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
-                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                    Completed = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime(6)", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Items", x => x.ItemId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CategoryItem",
+                columns: table => new
+                {
+                    CategoryItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ItemId = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryItem", x => x.CategoryItemId);
                     table.ForeignKey(
-                        name: "FK_Items_Categories_CategoryId",
+                        name: "FK_CategoryItem_Categories_CategoryId",
                         column: x => x.CategoryId,
                         principalTable: "Categories",
                         principalColumn: "CategoryId",
                         onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoryItem_Items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Items",
+                        principalColumn: "ItemId",
+                        onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateIndex(
-                name: "IX_Items_CategoryId",
-                table: "Items",
+                name: "IX_CategoryItem_CategoryId",
+                table: "CategoryItem",
                 column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryItem_ItemId",
+                table: "CategoryItem",
+                column: "ItemId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Items");
+                name: "CategoryItem");
 
             migrationBuilder.DropTable(
                 name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Items");
         }
     }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 
 namespace ToDoList.Models
 {
@@ -11,6 +12,9 @@ namespace ToDoList.Models
 
         public int ItemId { get; set; }
         public string Description { get; set; }
+        public bool Completed { get; set; }
+        public DateTime Date { get; set; }
+
 
         public virtual ICollection<CategoryItem> JoinEntities { get;}
     }
